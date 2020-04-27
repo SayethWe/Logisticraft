704cb261f30c5c41902d4855efae37bb5fa96632
Sayeth_We <powerplanets@aol.com>
2020-04-27 00:17:30 -0400

    Hardening Capsule, lang file, name refactoring

---
+++ src/main/java/com/sinesection/logisticraft/items/ItemHardeningCapsule.java
@@ -0,0 +1,20 @@
+package com.sinesection.logisticraft.items;
+
+import com.sinesection.logisticraft.Logisticraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemHardeningCapsule extends Item {
+    public ItemHardeningCapsule() {
+        setRegistryName("hardening_capsule");
+        setUnlocalizedName(Logisticraft.MODID+".hardening_capsule");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this,0,new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
